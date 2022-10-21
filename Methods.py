4a76c0115efd351a9e8445f26ce2f18dc06bcf7a
Dmitry_Tikhonov <tdmv1209@gmail.com>
2022-10-21 12:59:22 +0300

    Created all methods for programms

--- Methods.py
+++ Methods.py
@@ -1,7 +1,16 @@
 from random import randint
+import re
 
-def Create_list_random_int_from_0_to_9 (length):
+def enter (message):
+    num = int(input(message))
+    return num
+
+def сreate_list_random_num (length, min, max):
     random_list = []
     for item in range(length):
-        random_list.append(randint(0, 9))
+        random_list.append(randint(min, max + 1))
     return random_list
+
+def list_unrepeat_num (any_list):
+    any_list = list(set(any_list))
+    return any_list
